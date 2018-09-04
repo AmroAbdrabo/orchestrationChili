f0da157bea733b0dbebc424297c39a03e362fcd1
Ayberk Özgür <ayberk.ozgur@epfl.ch>
2018-09-04 10:06:26 +0200

    CelluloBluezUtil stubs

---
+++ src/comm/CelluloBluezUtil.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 EPFL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * @file CelluloBluezUtil.cpp
+ * @brief All things Cellulo done with the bluez c API
+ * @author Ayberk Özgür
+ * @date 2018-09-04
+ */
+
+#include "CelluloBluezUtil.h"
+
+namespace Cellulo{
+
+
+}
---
+++ src/comm/CelluloBluezUtil.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 EPFL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+/**
+ * @file CelluloBluezUtil.h
+ * @brief All things Cellulo done with the bluez c API
+ * @author Ayberk Özgür
+ * @date 2018-09-04
+ */
+
+#ifndef CELLULOBLUEZUTIL_H
+#define CELLULOBLUEZUTIL_H
+
+
+
+namespace Cellulo{
+
+
+/** @cond DO_NOT_DOCUMENT */
+
+/**
+ * @brief Object that exposes Cellulo functionalities that require the bluez c API
+ */
+class CelluloBluezUtil{
+
+public:
+
+
+
+};
+
+/** @endcond */
+
+}
+
+#endif // CELLULOBLUEZUTIL_H
